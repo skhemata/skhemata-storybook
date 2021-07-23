9f5f870a7a3020106650e7254580d413e663cff3
Ryan <ryan.pitts@skhemata.com>
2021-07-23 10:54:11 -0700

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+/**
+ * Define all stories that should be displayed in main storybook here
+ */
+
+const components = [
+//  "skhemata-editor-quill",
+//  "skhemata-google-analytics",
+  "skhemata-blog",
+  "skhemata-faq",
+  "skhemata-testimonial",
+  //"skhemata-contact",
+  "skhemata-subscription",
+  "skhemata-menu",
+  "skhemata-portfolio",
+  "skhemata-gdpr",
+  "skhemata-footer",
+  "skhemata-hero-banner",
+  "skhemata-pageloader",
+//  "skhemata-login",
+//  "skhemata-register",
+//  "skhemata-okta",
+];
+
+module.exports = {
+  stories: components.map((name) => 
+    `../node_modules/@skhemata/${name}/dist/stories/**/*.stories.{js,md,mdx}`
+  )
+};
